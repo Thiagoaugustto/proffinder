1bced9927b9180262b1375fa638efe7be69bbe4b
Thiago Barbosa <thiago.barbosa@aggrandize.com.br>
2024-10-29 11:35:18 -0300

    ...

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -3,9 +3,21 @@ import React from "react";
 import PageHeader from "../../components/PageHeader";
 import Input from "../../components/Input";
 
+import TeacherItem from "../../components/TeacherItem";
+
 import './styles.css';
 
 function TeacherList() {
+  const teacher = {
+    id: 1,
+    avatar: 'https://avatars.githubusercontent.com/u/46959139?v=4',
+    bio: 'Entusiasta das melhores tecnologias de química avançada.',
+    cost: 20,
+    name: 'Thiago Augusto',
+    subject: 'Química',
+    whatsapp: '9999999999'
+  };
+
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffinders disponíveis.">
@@ -15,6 +27,10 @@ function TeacherList() {
           <Input type="time" name="time" label="Hora" />
         </form>
       </PageHeader>
+
+      <main>
+        <TeacherItem teacher={teacher} />
+      </main>
     </div>
   )
 }
