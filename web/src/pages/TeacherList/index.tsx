7d96e830f2698813b720a0fffa895714f5ba65bd
Thiago Barbosa <thiago.barbosa@aggrandize.com.br>
2024-10-28 21:32:09 -0300

    ...

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 
+import PageHeader from "../../components/PageHeader";
+import Input from "../../components/Input";
+
+import './styles.css';
+
 function TeacherList() {
   return (
-    <h1>Teachers</h1>
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os proffinders disponíveis.">
+        <form id="search-teachers">
+          <Input name="subject" label="Matéria" />
+          <Input name="week_day" label="Dia da semana" />
+          <Input type="time" name="time" label="Hora" />
+        </form>
+      </PageHeader>
+    </div>
   )
 }
 
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,16 +1,18 @@
-import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Route, Routes as Router } from 'react-router-dom';
 
-import Landing from "./pages/Landing";
-import TeacherList from "./pages/TeacherList";
-import TeacherForm from "./pages/TeacherForm";
+import Landing from './pages/Landing';
+import TeacherList from './pages/TeacherList';
+import TeacherForm from './pages/TeacherForm';
 
 function Routes() {
   return (
     <BrowserRouter>
-      <Route path="/" element={<Landing />} />
-      <Route path="/study" element={<TeacherList />} />
-      <Route path="/give-classes" element={<TeacherForm />} />
+      <Router>
+        <Route path="/" element={<Landing />} />
+        <Route path="/study" element={<TeacherList />} />
+        <Route path="/give-classes" element={<TeacherForm />} />
+      </Router>
     </BrowserRouter>
   );
 }
